980ad2084977fac8e7a6195ae8fbfa66fc7140d0
Rohail Ramesh <rohailramesh@hotmail.com>
2025-01-28 15:41:54 +0000

    Feat: Auth controllers completed

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,38 +1,177 @@
-import bcryptjs from "bcryptjs";
+import bcrypt from "bcryptjs";
 import crypto from "crypto";
 import { User } from "../models/user.model.js";
-
+import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
+import {
+  sendPasswordResetEmail,
+  sendResetSuccessEmail,
+  sendVerificationEmail,
+  sendWelcomeEmail,
+} from "../mailtrap/emails.js";
 export const signup = async (req, res) => {
+  const { email, name, password } = req.body; //getting the email, password and name from the request body when this controller is activated
   try {
-  } catch (error) {}
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: "All fields are required." });
+    }
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be atleast 6 characters long." });
+    }
+    const userAlreadyExists = await User.findOne({ email });
+    if (userAlreadyExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    const salt = await bcrypt.genSalt(10); //generating salt to hash password with
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const verificationToken = Math.floor(
+      100000 + Math.random() * 900000
+    ).toString(); //generates a 6-digit random number as a string
+    const user = new User({
+      email,
+      password: hashedPassword,
+      name,
+      verificationToken,
+      verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000, //expires in 24hrs from the time this user signs up
+    });
+    await user.save();
+
+    // once user is created, create a jwt token by passing user id of this user as arg and set cookie
+    generateTokenAndSetCookie(res, user._id);
+    await sendVerificationEmail(user.email, verificationToken);
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({ success: false, message: message.error });
+  }
 };
 
 export const verifyEmail = async (req, res) => {
+  const { code } = req.body;
   try {
-  } catch (error) {}
+    const user = await User.findOne({
+      verificationToken: code,
+      verificationTokenExpiresAt: { $gt: Date.now() },
+    });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid verification code" });
+    }
+    user.isVerified = true;
+    user.verificationToken = undefined; //after verifying the email, delete the verification token
+    user.verificationTokenExpiresAt = undefined; //after verifying the email, delete the verification token
+    await user.save();
+    await sendWelcomeEmail(user.email, user.name);
+    res.status(200).json({ message: "Email verified successfully" });
+  } catch (error) {
+    res.status(400).json({ message: "Invalid verification code" });
+    console.log("error in verifyEmail controller", error);
+  }
 };
 
 export const login = async (req, res) => {
+  const { email, password } = req.body;
   try {
-  } catch (error) {}
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: "Invalid password" });
+    }
+    generateTokenAndSetCookie(res, user._id);
+    user.lastLogin = Date.now();
+    await user.save();
+    res.status(200).json({
+      success: true,
+      message: "User logged in successfully",
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({ success: false, message: message.error });
+    console.log("error in login controller", error);
+  }
 };
 
 export const logout = async (req, res) => {
   try {
-  } catch (error) {}
+    res.clearCookie("token");
+    res.status(200).json({ message: "User logged out successfully" });
+  } catch (error) {
+    console.log("error in logout controller", error);
+  }
 };
 
 export const forgotPassword = async (req, res) => {
+  const { email } = req.body;
   try {
-  } catch (error) {}
+    const username = await User.findOne({ email });
+    if (!username) {
+      return res.status(400).json({ message: "User not found" });
+    }
+    const resetToken = crypto.randomBytes(20).toString("hex"); // generates a random string of 20 characters
+    const resetTokenExpiresAt = Date.now() + 1 * 60 * 60 * 1000; //expires in 1hr
+    user.resetPasswordToken = resetToken;
+    user.resetPasswordExpiresAt = resetTokenExpiresAt;
+    await user.save();
+    await sendPasswordResetEmail(
+      user.email,
+      `${process.env.CLIENT_URL}/reset-password/${resetToken}`
+    );
+    res.status(200).json({ message: "Password reset email sent successfully" });
+  } catch (error) {
+    console.log("error in forgotPassword controller", error);
+  }
 };
 
 export const resetPassword = async (req, res) => {
   try {
-  } catch (error) {}
+    const { token } = req.params;
+    const { password } = req.body;
+    const user = await User.findOne({
+      resetPasswordToken: token,
+      resetPasswordExpiresAt: { $gt: Date.now() },
+    });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid reset token" });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    user.password = hashedPassword;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpiresAt = undefined;
+    await user.save();
+    await sendResetSuccessEmail(user.email);
+    res.status(200).json({ message: "Password reset successful" });
+  } catch (error) {
+    console.log("error in resetPassword controller", error);
+  }
 };
 
 export const checkAuth = async (req, res) => {
   try {
-  } catch (error) {}
+    const user = await User.findById(req.userId);
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+    res.status(200).json({
+      success: true,
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
+  } catch (error) {
+    console.log("error in checkAuth controller", error);
+  }
 };
